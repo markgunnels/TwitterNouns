d0d285963d06c56a6b1ade9f9e9d9d60b641d62a
Liz Favre <egfavre@gmail.com>
2016-11-16 15:15:22 -0500

    Twitter keys not passing OAuth

--- src/com/egfavre/Main.java
+++ src/com/egfavre/Main.java
@@ -1,8 +1,31 @@
 package com.egfavre;
 
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws TwitterException{
+        ConfigurationBuilder cf = new ConfigurationBuilder();
+
+        cf.setDebugEnabled(true)
+                .setOAuthConsumerKey("LHwwCO6Ti83khRDL5VOXCxHxf")
+                .setOAuthConsumerSecret("1UzGeUYaILaENN7Bq3Sn5TVJrTGyQYmlQDygWMnGfohW2bKNls")
+                .setOAuthAccessToken("798956880890626057-dErWME4TYvbduVPpd6jQ7Y5lHdfcOgr")
+                .setOAuthAccessTokenSecret("\trBHQvmWgiOTzAgG3vIVUvskSGgoASAXytYXTnHtw6xSkV");
+
+        TwitterFactory tf = new TwitterFactory(cf.build());
+        twitter4j.Twitter twitter = tf.getInstance();
+
+        List <Status> status = twitter.getHomeTimeline();
+        for (Status st: status){
+            System.out.println(st.getUser().getName() + "---" + st.getText());
+        }
+
+
     }
 }
